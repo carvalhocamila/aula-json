35df50c8033581914414994941edcba26ae1d4c1
carvalhocamila <101676220+carvalhocamila@users.noreply.github.com>
2022-04-25 22:10:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 $("#btnLogin").click(
     function(){
-
+        
         console.log("Essa é uma mensagem de log");
+        HTMLFormControlsCollection.log(
+            document.getElementById('InputEmail').value
+        
+        );
         
     }
 )
